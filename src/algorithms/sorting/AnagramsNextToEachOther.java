95085ba1d8c10c3b751e3c97d12f55e3c5294538
shravan-sambaraju <shravan.sambaraju@gmail.com>
2017-10-17 21:02:52 -0700

    more problems

---
+++ src/algorithms/sorting/AnagramsNextToEachOther.java
@@ -0,0 +1,34 @@
+package algorithms.sorting;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class AnagramsNextToEachOther {
+	public static void main(String[] args) {
+		String[] array = {"apple", "banana", "carrot", "ele", "duck", "papel", "tarroc", "cudk", "eel", "lee"};
+		System.out.println(stringArrayToString(array));
+		Arrays.sort(array, new AnagramComparator());
+		System.out.println(stringArrayToString(array));
+	}
+	
+	public static String stringArrayToString(String[] array) {
+		StringBuilder sb = new StringBuilder();
+		for (String v : array) {
+			sb.append(v + ", ");
+		}
+		return sb.toString();
+	}
+
+}
+
+ class AnagramComparator implements Comparator<String> {
+	private String sortChars(String s) {
+		char[] content = s.toCharArray();
+		Arrays.sort(content);
+		return new String(content);
+	}
+	
+    public int compare(String s1, String s2) {
+    	return sortChars(s1).compareTo(sortChars(s2));
+    }
+}
---
+++ src/problems/random/DivingBoardUsingKplanks.java
@@ -0,0 +1,24 @@
+package problems.random;
+
+import java.util.HashSet;
+
+public class DivingBoardUsingKplanks {
+	public static int counter = 0;
+	
+	public static HashSet<Integer> allLengths(int k, int shorter, int longer) {
+		counter++;
+		HashSet<Integer> lengths = new HashSet<Integer>();
+		for (int nShorter = 0; nShorter <= k; nShorter++) {
+			int nLonger = k - nShorter;
+			int length = nShorter * shorter + nLonger * longer;
+			lengths.add(length);
+		}
+		return lengths;
+	}
+	
+	public static void main(String[] args) {
+		HashSet<Integer> lengths = allLengths(12, 1, 3);
+		System.out.println(lengths.toString());
+	}
+
+}
