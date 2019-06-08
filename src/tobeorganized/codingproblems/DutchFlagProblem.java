bfc7d57c258a7306659a3eea4a1d85080bbdfc48
sks8hz5 <shravan_k_sambaraju@homdepot.com>
2019-06-08 13:05:19 -0400

    adding changes

---
+++ src/tobeorganized/codingproblems/DutchFlagProblem.java
@@ -0,0 +1,38 @@
+package tobeorganized.codingproblems;
+
+import java.util.Arrays;
+
+public class DutchFlagProblem {
+
+  public static void sortDutchFlag(int[] array) {
+    int low = 0, high = array.length - 1, mid = 0, temp = 0;
+    while (mid <= high) {
+      switch (array[mid]) {
+        case 0:
+          temp = array[low];
+          array[low] = array[mid];
+          array[mid] = temp;
+          low++;
+          mid++;
+          break;
+
+        case 1:
+          mid++;
+          break;
+
+        case 2:
+          temp = array[mid];
+          array[mid] = array[high];
+          array[high] = temp;
+          high--;
+          break;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] array = {1, 2, 0, 0, 0, 1, 2, 0, 0, 1, 2};
+    sortDutchFlag(array);
+    System.out.println(Arrays.toString(array));
+  }
+}
