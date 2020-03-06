8072354ee26bc32d4b9ed6d30284f4fbe5b4045e
shravan sambaraju <shravansambaraju@shravans-MacBook-Pro.local>
2020-03-06 17:15:48 -0600

    refactoring

--- src/datastructures/CircularQueueImplementation.java
+++ src/datastructures/CircularQueueImplementation.java
@@ -70,8 +70,8 @@ public class CircularQueueImplementation {
 }
 
 /**
- * Your CircularQueueImplementation object will be instantiated and called as such: MyCircularQueue obj = new
- * MyCircularQueue(k); boolean param_1 = obj.enQueue(value); boolean param_2 = obj.deQueue(); int
- * param_3 = obj.Front(); int param_4 = obj.Rear(); boolean param_5 = obj.isEmpty(); boolean param_6
- * = obj.isFull();
+ * Your CircularQueueImplementation object will be instantiated and called as such: MyCircularQueue
+ * obj = new MyCircularQueue(k); boolean param_1 = obj.enQueue(value); boolean param_2 =
+ * obj.deQueue(); int param_3 = obj.Front(); int param_4 = obj.Rear(); boolean param_5 =
+ * obj.isEmpty(); boolean param_6 = obj.isFull();
  */
