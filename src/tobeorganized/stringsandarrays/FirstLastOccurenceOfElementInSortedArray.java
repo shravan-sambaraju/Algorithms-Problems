4ac3af7c8bd9d68b3c1f621ba4d121aad8607188
shravan sambaraju <shravansambaraju@shravans-MacBook-Pro.local>
2020-04-01 11:03:29 -0500

    adding changes

--- src/tobeorganized/stringsandarrays/FirstLastOccurenceOfElementInSortedArray.java
+++ src/tobeorganized/stringsandarrays/FirstLastOccurenceOfElementInSortedArray.java
@@ -45,8 +45,45 @@ public class FirstLastOccurenceOfElementInSortedArray {
     return idx;
   }
 
+  public static int[] searchRangeRecursion(int[] nums, int target) {
+    int[] result = new int[2];
+    if (nums == null || nums.length == 0) {
+      return result;
+    }
+    result[0] = binarySearchHelper(nums, 0, nums.length - 1, target, true);
+    result[1] = binarySearchHelper(nums, 0, nums.length - 1, target, false);
+    return result;
+  }
+
+  private static int binarySearchHelper(
+      int[] nums, int low, int high, int target, Boolean findFirst) {
+    if (low > high) {
+      return -1;
+    }
+    int mid = low + (high - low) / 2;
+    if (findFirst) {
+      if ((mid == 0 || nums[mid - 1] < target) && nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        return binarySearchHelper(nums, mid + 1, high, target, findFirst);
+      } else {
+        return binarySearchHelper(nums, low, mid - 1, target, findFirst);
+      }
+    } else {
+      if ((mid == nums.length - 1 || nums[mid + 1] > target) && nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        return binarySearchHelper(nums, low, mid - 1, target, findFirst);
+      } else {
+        return binarySearchHelper(nums, mid + 1, high, target, findFirst);
+      }
+    }
+  }
+
   public static void main(String[] args) {
     System.out.println(Arrays.toString(searchRange(new int[] {5, 7, 7, 8, 8, 10}, 8)));
     System.out.println(Arrays.toString(searchRange(new int[] {5, 7, 7, 8, 10}, 6)));
+    System.out.println(Arrays.toString(searchRangeRecursion(new int[] {5, 7, 7, 8, 8, 10}, 8)));
+    System.out.println(Arrays.toString(searchRangeRecursion(new int[] {5, 7, 7, 8, 10}, 6)));
   }
 }
